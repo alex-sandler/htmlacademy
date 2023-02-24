81c66a2c4dc14f53c507820ac32ce36b1928b231
alex-sandler <troubadour1488@gmail.com>
2023-02-24 22:47:55 +0300

    Знакомимся с обработчиком событий

--- Основы JavaScript/script.js
+++ Основы JavaScript/script.js
@@ -1,3 +1,8 @@
 let page = document.querySelector('.page');
 page.classList.remove('light-theme');
 page.classList.add('dark-theme');
+
+let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+ console.log('Кнопка нажата!') 
+}
