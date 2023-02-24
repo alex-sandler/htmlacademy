cb77d52237e2e2f925c2170452ec51d8c671b240
alex-sandler <troubadour1488@gmail.com>
2023-02-24 22:56:03 +0300

    Знакомимся с classList.toggle

--- Основы JavaScript/script.js
+++ Основы JavaScript/script.js
@@ -3,8 +3,5 @@ let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 
 themeButton.onclick = function() {
-  console.log('Кнопка нажата!');
-  page.classList.remove('light-theme');
-  page.classList.add('dark-theme');
-  
+  page.classList.toggle('light-theme');
 };
