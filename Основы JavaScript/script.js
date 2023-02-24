ccd9c4e1e97f7b94b60f967a2b7408fe028cb390
alex-sandler <troubadour1488@gmail.com>
2023-02-24 23:07:23 +0300

    Знакомимся со свойством textContent

--- Основы JavaScript/script.js
+++ Основы JavaScript/script.js
@@ -6,3 +6,7 @@ themeButton.onclick = function() {
   page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme');
 };
+
+let message = document.querySelector('.subscription-message');
+
+console.log(message.textContent);
