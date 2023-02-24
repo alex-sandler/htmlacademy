1523e1cb7e3cf46e5361d9d5b477b3e118674db3
alex-sandler <troubadour1488@gmail.com>
2023-02-24 23:51:54 +0300

    Используем конкатенацию

--- Основы JavaScript/script.js
+++ Основы JavaScript/script.js
@@ -10,10 +10,11 @@ themeButton.onclick = function() {
 let message = document.querySelector('.subscription-message');
 
 let form = document.querySelector('.subscription');
+
 let email = document.querySelector('.subscription-email');
 
 form.onsubmit = function(evt) {
   evt.preventDefault();
   // Измените значение textContent на следующей строке
-  message.textContent = email.value;
+  message.textContent = 'Адрес ' + email.value + ' добавлен в список получателей рассылки.';
 };
